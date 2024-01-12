e6019b08495ed56a067c26d9b71991a12603c2be
João Paulo Dagostin <dagostinjota@gmail.com>
2024-01-11 22:48:02 -0300

    Update Cabecalho.test.js

--- src/componentes/Cabecalho/Cabecalho.test.js
+++ src/componentes/Cabecalho/Cabecalho.test.js
@@ -1,4 +1,8 @@
-test('Primeiro teste', () => {
-  const numero = 10;
-  expect(numero).toBe(10);
+import { render, screen } from '@testing-library/react';
+import Cabecalho from './index';
+
+test('Deve renderizar o nome do  usuario logado', () => {
+  render(<Cabecalho />);
+  const nomeUsuario = screen.getByText('Joana Fonseca Dutra');
+  expect(nomeUsuario).toBeInTheDocument();
 });

ce7add17590d8708cca800fb05f0fe7e8b42d1cd
João Paulo Dagostin <dagostinjota@gmail.com>
2024-01-15 20:35:26 -0300

    testing
    novo saldo

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -1,3 +1,27 @@
+import { calculaNovoSaldo } from './index';
+
+describe('Quando eu realizo uma transacao', () => {
+  test('Que e um deposito, o saldo deve aumentar', () => {
+    const transacao = {
+      transacao: 'Deposito',
+      valor: 50,
+    };
+
+    const novoSaldo = calculaNovoSaldo(transacao, 100);
+    expect(novoSaldo).toBe(150);
+  });
+});
+
+test('Que e uma transferencia, o saldo deve diminuir', () => {
+  const transacao = {
+    transacao: 'Transferencia',
+    valor: 50,
+  };
+
+  const novoSaldo = calculaNovoSaldo(transacao, 100);
+  expect(novoSaldo).toBe(150);
+});
+
 test('Deve retornar o valor do saldo atualizado com o rendimetno', () => {
   const calculaRendimento = jest.fn((saldo) => saldo + saldo * 0.005);
 
